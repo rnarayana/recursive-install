316b542c74089d720d4c4a834cce30deff972dcc
Narayana Rengaswamy <narayana.rengaswamy@optym.com>
2020-05-20 12:31:14 +0530

    Fix for running in Windows

--- recursive-npm.js
+++ recursive-npm.js
@@ -1,60 +1,69 @@
 #!/usr/bin/env node
 
-var path = require('path')
-var shell = require('shelljs')
-var execSync = require('child_process').execSync
-var argv = require('yargs').argv
+const path = require("path");
+const shell = require("shelljs");
+const execSync = require("child_process").execSync;
+const argv = require("yargs").argv;
 
-function noop (x) { return x }
-
-function getPackageJsonLocations (dirname) {
-  return shell.find(dirname)
-    .filter(function (fname) {
-      return !(fname.indexOf('node_modules') > -1 || fname[0] === '.') &&
-        path.basename(fname) === 'package.json'
-    })
-    .map(function (fname) {
-      return path.dirname(fname)
-    })
+function noop(x) {
+  return x;
 }
 
-function npmInstall (dir) {
+function getPackageJsonLocations(dirname) {
+  return shell
+    .find(dirname)
+    .filter(function (fname) {
+      return (
+        !(fname.indexOf("node_modules") > -1 || fname[0] === ".") &&
+        path.basename(fname) === "package.json"
+      );
+    })
+    .map(function (fname) {
+      return path.dirname(fname);
+    });
+}
+
+function npmInstall(dir) {
   var exitCode = 0;
   try {
-    if(argv.production) {
-      console.log('Installing ' + dir + '/package.json with --production option')
-      execSync('npm install --production', { cwd: dir})
+    if (argv.production) {
+      console.log(
+        "Installing " + dir + "/package.json with --production option"
+      );
+      execSync("npm install --production", { cwd: dir });
     } else {
-      console.log('Installing ' + dir + '/package.json')
-      execSync('npm install', { cwd: dir})
+      console.log("Installing " + dir + "/package.json");
+      execSync("npm install", { cwd: dir });
     }
-    console.log('')
+    console.log("");
   } catch (err) {
-    exitCode = err.status
+    exitCode = err.status;
   }
 
   return {
     dirname: dir,
-    exitCode: exitCode
-  }
+    exitCode: exitCode,
+  };
 }
 
-function filterRoot (dir) {
+function filterRoot(dir) {
   if (path.normalize(dir) === path.normalize(process.cwd())) {
-    console.log('Skipping root package.json')
-    return false
+    console.log("Skipping root package.json");
+    return false;
   } else {
-    return true
+    return true;
   }
 }
 
 if (require.main === module) {
-  var exitCode = getPackageJsonLocations(argv.rootDir ? argv.rootDir : process.cwd())
+  var exitCode = getPackageJsonLocations(
+    argv.rootDir ? argv.rootDir : process.cwd()
+  )
     .filter(argv.skipRoot ? filterRoot : noop)
     .map(npmInstall)
     .reduce(function (code, result) {
-      return result.exitCode > code ? result.exitCode : code
-    }, 0)
+      return result.exitCode > code ? result.exitCode : code;
+    }, 0);
 
-  process.exit(exitCode)
+  process.exit(exitCode);
 }
